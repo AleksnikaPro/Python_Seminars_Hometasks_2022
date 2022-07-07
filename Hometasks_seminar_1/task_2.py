d1e9ab73ca57929582239a1ea4f5c4238bb448eb
Aleksandra Popova <alexnika@bk.ru>
2022-07-07 20:25:58 +0300

    changed Hometasks_seminar_1/task_2.py

--- Hometasks_seminar_1/task_2.py
+++ Hometasks_seminar_1/task_2.py
@@ -1,4 +1,11 @@
 # 2.Напишите программу для проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-
+check = [0, 1]
+for x in check:
+    for y in check:
+        for z in check:
+            if not(x or y or z) == (not x and not y and not z):
+                print(f"{x}, {y}, {z} true")
+            else:
+                print(f"{x}, {y}, {z} false")
