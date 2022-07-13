6caf3f8402b5ac55e7e49e5fd386cc9fca182acd
Aleksandra Popova <alexnika@bk.ru>
2022-07-13 14:28:01 +0300

    changed Hometasks_seminar_2/task_5.py

--- Hometasks_seminar_2/task_5.py
+++ Hometasks_seminar_2/task_5.py
@@ -14,3 +14,22 @@ list = [18, 12, 5, 11, 10, 3]
 print(f'Исходный список {list}')
 print(f'Перемешанный список {shuffle_list(list)}')
 
+
+#______________________________________________________
+
+# def fill_list(arg_1):
+#     my_list = []
+#     for index in range(arg_1):
+#         my_list.append(random.randint(0, 30))
+#     return my_list
+
+# def shuffled_list(arg_1):
+#     for i in range(len(arg_1)-1, 0, -1):
+#         j = random.randint(0, i + 1)
+#         arg_1[i], arg_1[j] = arg_1[j], arg_1[i]
+#     print(f'Перемешанный список: {arg_1}')
+
+# number = int(input('Задайте длину списка: '))
+# filled_list = fill_list(number)
+# print(f'Исходный список: {filled_list}')
+# shuffled_list(filled_list)
